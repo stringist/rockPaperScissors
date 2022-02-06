1b82be6edf02cdccb13d48cc6426c5e70902f69d
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-02-06 21:19:22 +0100

    done with strings presentation exercises

--- strings.js
+++ strings.js
@@ -41,3 +41,49 @@ const petName = "Tapio";
 
 console.log(`My name is ${firstName}.
 I have a ${petSpecies} called ${petName}.`);
+
+
+// String methods - like a function that belongs to an object, and affects the object in some way
+// .trim - removes space  and line breaks from beginning and end of string
+
+const str1 = " There is   space here \n  wow  \n  "
+const str2 = str1.trim();
+
+// underscores make it easier to see if there are spaces
+
+console.log(`_${str1}_`);
+console.log(`__${str2}_`);
+
+// method parameters
+const exampleFullName = "Peter Heronimous Lind";
+const exampleFirstName = exampleFullName.substring(0, 5);
+const lastSomething = exampleFullName.substring(5);
+
+console.log(exampleFullName, exampleFirstName + "_", "_" + lastSomething);
+
+// substring exercises
+const wizard = "Albus Percival Wulfric Brian Dumbledore";
+const totalLength = wizard.length;
+console.log(`The total number of characters is ${totalLength}.`);
+
+const index2 = wizard[2];
+console.log(`The character at index 2 is ${index2}.`);
+
+const index6 = wizard[6];
+console.log(`The character at index 6 is ${index6}.`);
+
+const firstD = wizard.length - 10;
+console.log(`the index of the first "D" in "dumbledore" is ${firstD}`);
+console.log(wizard.indexOf("D"));
+
+const lastLetter = wizard[wizard.length - 1];
+console.log(`The index of the last 'e' in 'Dumbledore' is ${totalLength}.`);
+// why doesn't this match?
+console.log(wizard.lastIndexOf("e"));
+
+console.log("_" + wizard.substring(0, 5) + "_");
+console.log("_" + wizard.substring(29) + "_");
+console.log("_" + wizard.substring(15, (15 + 7)) + "_");
+console.log("_" + wizard.substring(14, (15 + 8)) + "_");
+console.log("_" + wizard.substring(25, 28) + "_");
+console.log("_" + wizard.substring(2, 5) + "_");
